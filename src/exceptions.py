685dc37f67d0eec38410f20fd24e49e7004293d4
Raghav Wahi <raghavwahi7@gmail.com>
2023-10-07 13:22:24 +0530

    - exceptions.py: Added Exception class for DoublyLinkedList

---
+++ src/exceptions.py
@@ -0,0 +1,6 @@
+"""This module contains custom exception classes"""
+
+class DoublyLinkedListError(Exception):
+    """
+    This class depicts the exceptions raised from DoublyLinkedList class.
+    """
